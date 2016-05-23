1c7630efc60c56cdb941a4c5e89c7d9121cbd988
Cheung, Wilson <wilson.cheung@cengage.com>
2016-05-23 13:53:47 -0700

    init

---
+++ Readme.md
@@ -0,0 +1,36 @@
+# Budgeting - React + Redux Sample App
+
+Sample app demonstrating the power and simplicity of React, Redux, and Webpack 2. Includes tree shaking configuration. 
+
+This is a Webpack 2 version of a previous [Budgeting App](https://github.com/ModusCreateOrg/budgeting-sample-app).
+
+## Contains
+
+- [x] [Webpack 2](https://webpack.github.io)
+- [x] [React](https://facebook.github.io/react/)
+- [x] [Redux](https://github.com/rackt/redux)
+- [x] [Babel](https://babeljs.io/)
+
+Make sure you have the latest node before using Webpack 2. Some depenencies my fail in older versions. 
+
+## Setup
+
+```
+$ npm install
+```
+
+## Running
+
+```
+$ npm start
+```
+
+## Build
+
+```
+$ npm run build
+```
+
+# License
+
+MIT
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = nodeEnv === 'production';
+
+module.exports = {
+  devtool: isProd ? 'hidden-source-map' : 'cheap-eval-source-map',
+  context: path.join(__dirname, './client'),
+  entry: {
+    index: './index.js',
+    exports: './exports.js',
+    vendor: ['react','react-dom']
+  },
+  output: {
+    path: path.join(__dirname, './static'),
+    filename: '[name].bundle.js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'file',
+        query: {
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style',
+          'css'
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: [
+          // 'react-hot',
+          'babel-loader'
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    modules: [
+      path.resolve('./client'),
+      'node_modules'
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.bundle.js'
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+    })
+  ],
+  devServer: {
+    contentBase: './client'
+    // hot: true
+  }
+};
